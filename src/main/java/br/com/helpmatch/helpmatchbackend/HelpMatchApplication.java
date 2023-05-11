5ff2ddeefcd7e163fc6dff854a29e249844a220d
iurysalino <iurysalino@gmail.com>
2023-05-11 01:03:02 -0300

    config: create base project

---
+++ src/main/java/br/com/helpmatch/helpmatchbackend/HelpMatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.helpmatch.helpmatchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/helpmatch/helpmatchbackend/HelpMatchApplicationTests.java
@@ -0,0 +1,7 @@
+package br.com.helpmatch.helpmatchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpMatchApplicationTests {}
