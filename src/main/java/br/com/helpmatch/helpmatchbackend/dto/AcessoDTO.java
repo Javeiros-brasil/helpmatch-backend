233c9f98bfa3e67db483e92b27dd0cbe38d06a09
nicolas-trindade <nicolastrindade@hotmail.com>
2023-06-10 18:02:19 -0300

    data no dto

--- src/main/java/br/com/helpmatch/helpmatchbackend/dto/AcessoDTO.java
+++ src/main/java/br/com/helpmatch/helpmatchbackend/dto/AcessoDTO.java
@@ -1,34 +1,17 @@
 package br.com.helpmatch.helpmatchbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class AcessoDTO {
 
     private String username;
     private String password;
 
-    public AcessoDTO() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "AcessoDTO{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
